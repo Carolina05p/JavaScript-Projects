6d250ec688069af0feaa1d43fd752e5de2ecd3c9
Carolina05p <107445794+Carolina05p@users.noreply.github.com>
2022-07-25 11:39:11 -0400

    Added index file

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,9 @@
+var A = "This is the beggining of the string"; //Adding a string value
+window.alert(A);
+document.write("This is a string for the \"Assignment Escape Character\"")
+document.write("\"Your time is limited, so don\'t waste it living someone else\'s life. " + "Don\'t be trapped by dogma, which is living with the results of other people\'s thinking.\" " + "-Steve Jobs");
+var B = "This is the end of the string"
+document.write(B)
+var Cats = "The Aristocats", Dad = "Thomas O'Malley", Mom = "Duchess", Daughter = "Marie", Son = "Toulouse", Son = "Berlioz";
+document.write(Mom) //Assign a variable from a statement with multiple variables
+document.write(3+3); //Statement that contains an expression (3+3)
