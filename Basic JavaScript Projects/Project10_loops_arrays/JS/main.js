96ec9ce5ac461852a6acd373c2e067d9457fb898
Carolina05p <107445794+Carolina05p@users.noreply.github.com>
2022-08-08 15:05:53 -0400

    Added project 10

--- Basic JavaScript Projects/Project10_loops_arrays/JS/main.js
+++ Basic JavaScript Projects/Project10_loops_arrays/JS/main.js
@@ -17,3 +17,44 @@ function for_Loop() {
     }
     document.getElementById("List_of_Instruments").innerHTML = Content;
 }
+
+function array_Function() {
+    var Dog_Picture = [];
+    Dog_Picture [0] = "playing";
+    Dog_Picture [1] = "sleeping";
+    document.getElementById("Array").innerHTML = "Here, the dog is " + Dog_Picture [1] + ".";
+}
+
+function constant_function() {
+    const Animal = {type:"dog", name:"Brandy", color:"brown"};
+    Animal.color = "black";
+    Animal.price = "$3,000";
+    document.getElementById("Constant").innerHTML = "The price for the " +
+        Animal.type + " is " + Animal.price;
+}
+
+let x = 10;
+// Here x is 10
+
+{
+let x = 2;
+// Here x is 2
+}
+
+// Here x is 10
+
+function myFunction() {
+    return Math.PI;
+  }
+  document.getElementById("return").innerHTML = myFunction();
+
+  let car = {
+    make:"Nissan",
+    model:"Rogue",
+    year:"2018",
+    color:"White",
+    description: function() {
+        return "This car is a " + this.year + this.color + this.make + this.model;
+    }
+  };
+  document.getElementById("Car_Object").innerHTML = car.description();
